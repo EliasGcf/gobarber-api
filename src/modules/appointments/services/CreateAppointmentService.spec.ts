1b9c0cda87f6e8422b5af835d4f13b87257b2a25
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-05-12 17:58:35 -0300

    test: fix past date error

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -29,7 +29,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 4, 10, 11);
+    const appointmentDate = new Date(2021, 4, 10, 11);
 
     await createAppointment.execute({
       provider_id: 'provider_id',
