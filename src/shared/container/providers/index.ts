5503e17ba61e474d444484ceade41591c55a0481
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-05-13 12:51:27 -0300

    refactor: change the responsibility to initialize the containers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from './StorageProvider/models/IStorageProviders';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailTempalteProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTempalteProvider>(
-  'MailTempalteProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
