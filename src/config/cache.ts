c47e20b43673491008e7a8e0331090697a44c095
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-05-13 21:50:27 -0300

    feat: add express rate limit middleware

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -13,9 +13,9 @@ export default {
 
   config: {
     redis: {
-      host: '192.168.0.103',
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
