5796b656a223b9956ea6cdb3af8de2feca6ce640
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-05-21 22:48:01 -0300

    feat: add serialization when save in cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   provider_id: string;
@@ -43,7 +44,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
